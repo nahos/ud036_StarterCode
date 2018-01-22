87134f049d3f2d6a4161f6f858d0c63d6440b4b0
Sohan Sathyan <sohansathyan14@gmail.com>
2018-01-22 22:10:35 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,12 +1,16 @@
-#Creates a class movie
-import webbrowser 
+# Creates a class movie
+import webbrowser
+
+
 class Movie():
-    #Below is the constructor for Movie Class
-    def __init__(self, movie, storyline, image, trailer):#constructor, self is the object being created
+    # Below is the constructor for Movie Class
+    def __init__(self, movie, storyline, image, trailer):
+        # constructor, self is the object being created
         self.title = movie
         self.storyline = storyline
         self.poster_image_url = image
         self.trailer_youtube_url = trailer
-	#Below Function shows the trailer of the movie instance
+
+    # Below Function shows the trailer of the movie instance
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
